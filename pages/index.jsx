578c5fb8ab792ebde974511e65be279f956d0054
FelixTellmann <Fr10tz1!1>
2020-01-23 22:55:24 +0200

    Initial Commit..

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdSearch, MdCancel } from 'react-icons/md';
+import { MdSearch, MdCancel, MdWeb } from 'react-icons/md';
 
 import Link from 'next/link';
 
@@ -18,14 +18,237 @@ export default () => (
 
       </nav>
       <div className="layout__search search">
-        <input className="search__input" type="text" placeholder="Search" required/>
+        <input className="search__input" type="text" placeholder="Search" required />
         <MdSearch className="search__search-icon" />
         <button className="search__cancel"><MdCancel className="search__cancel-icon" /></button>
         <button className="search__submit">Submit</button>
       </div>
     </header>
     <aside className="layout__aside">
-      side navigation
+      <nav className="secondary-nav">
+        <a href="#" className="secondary-nav__link">
+          <MdWeb className="secondary-nav__icon" />
+          <span className="secondary-nav__text">Structure</span>
+        </a>
+        <ul className="subnav">
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Frame</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Page</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Page Action</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Layout</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Empty State</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Stack</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Card</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="subnav__item">
+            <a href="#" className="subnav__link">
+              <span className="subnav__text">Callout Card</span>
+            </a>
+            <ul className="subnav__list">
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Examples</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Bext Practices</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Content Guidelines</span>
+                </a>
+              </li>
+              <li className="subnav__list__item">
+                <a href="#" className="subnav__list__link">
+                  <span className="subnav__list__text">Related Components</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </aside>
     <main className="layout__main">
       Content
